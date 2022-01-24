62a2891415eb4549a415469a27d23673d5135218
Aleks Petrov <mr.alex.petrov@gmail.com>
2022-01-24 17:26:27 +0100

    sImplify media component

--- components/atoms/media.vue
+++ components/atoms/media.vue
@@ -1,36 +1,28 @@
 <script setup>
+// Very basic implemenation of media component
 import { computed } from 'vue'
 
 const props = defineProps({
   src: { type: String, default: '' },
   alt: { type: String, default: '' },
-  sizes: { type: String, default: '100vw' },
+  sizes: { type: Array, default: () => ['320', '640', '768', '1024', '1280'] },
   name: { type: String, default: '' },
   quality: { type: Number, default: 70 },
-  width: { type: Number, default: 'auto' },
-  height: { type: Number, default: 'auto' },
 })
 
-const screens = ['320', '640', '768', '1024', '1280']
-const claudinaryUrl = 'https://res.cloudinary.com/alekspetrov/image/upload'
-const src = `${claudinaryUrl}/f_auto,q_70,w_640/v1642683138/website/${props.name}.jpg`
+const url = 'https://res.cloudinary.com/alekspetrov/image/upload'
+const src = `${url}/f_auto,q_70,w_640/v1642683138/website/${props.name}.jpg`
+
 const srcset = computed(() =>
-  screens.map(
+  props.sizes.map(
     screen =>
-      `${claudinaryUrl}/f_auto,q_${props.quality},w_${screen}/v1642683138/website/${props.name}.jpg ${screen}w`
+      `${url}/f_auto,q_${props.quality},w_${screen}/v1642683138/website/${props.name}.jpg ${screen}w`
   )
 )
 </script>
 
 <template>
   <figure>
-    <img
-      :sizes="sizes"
-      :srcset="srcset.join(', ')"
-      :src="src"
-      :alt="alt"
-      :width="width"
-      :height="height"
-    />
+    <img :sizes="sizes" :srcset="srcset.join(', ')" :src="src" :alt="alt" />
   </figure>
 </template>
