9b52ff6e409f2b10cb335b7798c32bdd27732c85
Aleks Petrov <mr.alex.petrov@gmail.com>
2022-02-22 10:17:20 +0100

    upd: cf web analitics script

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,15 @@ import { defineNuxtConfig } from 'nuxt3'
 import eslintPlugin from 'vite-plugin-eslint'
 
 export default defineNuxtConfig({
+  meta: {
+    script: [
+      {
+        src: 'https://static.cloudflareinsights.com/beacon.min.js',
+        'data-cf-beacon': '{"token": "9d53d29ccc2c4bb9a5530ca79c07efb8"}',
+        defer: true,
+      },
+    ],
+  },
   css: [
     '~/assets/css/reset.css',
     '~/assets/css/tokens.css',
